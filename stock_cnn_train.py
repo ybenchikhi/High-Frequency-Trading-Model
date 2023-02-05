ad1fe1b622d26af6ee1c414911474a5e5e6c6845
Youssef Benchikhi <97771549+ybenchikhi@users.noreply.github.com>
2023-02-05 18:35:45 -0500

    Update stock_cnn_train.py

--- stock_cnn_train.py
+++ stock_cnn_train.py
@@ -4,36 +4,32 @@ import pandas as pd
 import yfinance as yf
 import subprocess
 
-# Download the real-time data for a stock from Yahoo Finance
-ticker = "AAPL"
-df = yf.download(ticker, period="1m", interval="1m")
+# Define a function to download and preprocess data for a given stock
+def get_data(ticker):
+    df = yf.download(ticker, period="1m", interval="1m")
+    volatility = df['Close'].rolling(5).std()
+    X = np.array(volatility[-6:-1]).reshape(1, -1)
+    return X
 
-# Preprocess the data
-volatility = df['Close'].rolling(5).std()
-X = np.array(volatility[-6:-1]).reshape(1, -1)
-
-# Define the model architecture
+# Load the pre-trained model
 model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(64, activation='relu', input_shape=(5, )),
     tf.keras.layers.Dense(32, activation='relu'),
     tf.keras.layers.Dense(16, activation='relu'),
     tf.keras.layers.Dense(1, activation='sigmoid')
 ])
-
-# Compile the model with binary crossentropy loss and the Adam optimizer
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-# Load the pre-trained weights
 model.load_weights("model_weights.h5")
 
-# Make predictions on the new data
-predictions = model.predict(X)
+# Define a list of stock tickers to check
+tickers = ["AAPL", "GOOG", "MSFT", "TSLA", "AMZN"]
 
-# Check if the model predicts a high enough return to warrant buying the stock
-if predictions[0] > 0.5:
-    # Call the C++ file to execute the trade
-    subprocess.call(["./ultra_low_latency_trading_model", ticker])
-    print("Bought stock")
-else:
-    # Don't buy the stock
-    print("Not buying stock")
+# Iterate over the list of tickers
+for ticker in tickers:
+    X = get_data(ticker)
+    predictions = model.predict(X)
+    if predictions[0] > 0.5:
+        subprocess.call(["./ultra_low_latency_trading_model", ticker])
+        print("Bought stock", ticker)
+    else:
+        print("Not buying stock", ticker)
