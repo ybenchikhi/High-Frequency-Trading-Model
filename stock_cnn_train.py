e537e6319a5dd5c1fbf9fd1422541e39680a7ae5
Youssef Benchikhi <97771549+ybenchikhi@users.noreply.github.com>
2023-02-05 18:29:25 -0500

    Update stock_cnn_train.py

--- stock_cnn_train.py
+++ stock_cnn_train.py
@@ -1,39 +1,39 @@
+import tensorflow as tf
 import numpy as np
 import pandas as pd
-import tensorflow as tf
-from keras.layers import Conv1D, Dense, Flatten, MaxPooling1D
-from keras.models import Sequential
+import yfinance as yf
+import subprocess
 
-# Load the stock price data into a Pandas DataFrame
-df = pd.read_csv("stock_prices.csv")
+# Download the real-time data for a stock from Yahoo Finance
+ticker = "AAPL"
+df = yf.download(ticker, period="1m", interval="1m")
 
-# Preprocess the data by normalizing it
-data = df.values
-data = (data - np.mean(data, axis=0)) / np.std(data, axis=0)
+# Preprocess the data
+volatility = df['Close'].rolling(5).std()
+X = np.array(volatility[-6:-1]).reshape(1, -1)
 
-# Split the data into training and testing sets
-train_data = data[:int(0.8 * len(data))]
-test_data = data[int(0.8 * len(data)):]
-
-# Define the CNN model
-model = Sequential([
-    Conv1D(32, 5, activation='relu', input_shape=(len(df.columns), 1)),
-    MaxPooling1D(2),
-    Conv1D(64, 5, activation='relu'),
-    MaxPooling1D(2),
-    Flatten(),
-    Dense(128, activation='relu'),
-    Dense(1, activation='sigmoid')
+# Define the model architecture
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(64, activation='relu', input_shape=(5, )),
+    tf.keras.layers.Dense(32, activation='relu'),
+    tf.keras.layers.Dense(16, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
 ])
 
-# Compile the model
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+# Compile the model with binary crossentropy loss and the Adam optimizer
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# Train the model
-train_data = np.expand_dims(train_data, axis=-1)
-model.fit(train_data, train_labels, epochs=10)
+# Load the pre-trained weights
+model.load_weights("model_weights.h5")
 
-# Evaluate the model on the test data
-test_data = np.expand_dims(test_data, axis=-1)
-test_loss, test_accuracy = model.evaluate(test_data, test_labels)
-print("Test Accuracy:", test_accuracy)
+# Make predictions on the new data
+predictions = model.predict(X)
+
+# Check if the model predicts a high enough return to warrant buying the stock
+if predictions[0] > 0.5:
+    # Call the C++ file to execute the trade
+    subprocess.call(["./ultra_low_latency_trading_model", ticker])
+    print("Bought stock")
+else:
+    # Don't buy the stock
+    print("Not buying stock")
